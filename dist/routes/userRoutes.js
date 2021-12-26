efca634a054d64c092ff3b86180a1e2c54bb6490
Professordev7 <professor.dev.run@gmail.com>
2021-12-26 03:50:50 +0330

    Update gitignore

--- dist/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var userController_1 = require("../controllers/userController");
-var router = express_1.Router();
-router.post('/', userController_1.registerUser);
-router.post('/info', userController_1.getUser);
-exports.default = router;
--- dist/routes/users.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var userController_1 = require("../controllers/userController");
-var router = express_1.Router();
-router.post('/', userController_1.registerUser);
